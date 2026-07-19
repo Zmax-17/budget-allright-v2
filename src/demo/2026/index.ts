31e2af7a90dc41c6af675f738c76322bc98fed6c
Zmax-17 <maxky@ukr.net>
2026-07-19 16:50:46 +0200

    feat: add demo transactions for 2026 Q3

--- src/demo/2026/index.ts
+++ src/demo/2026/index.ts
@@ -1,8 +1,10 @@
 import { DemoTransactions } from "../types";
 import { demo2026Q1 } from "./q1";
 import { demo2026Q2 } from "./q2";
+import { demo2026Q3 } from "./q3";
 
 export const demo2026: DemoTransactions = [
   ...demo2026Q1,
   ...demo2026Q2,
+  ...demo2026Q3,
 ] as const;
